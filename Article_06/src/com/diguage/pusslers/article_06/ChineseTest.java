18b3eaeccbc3f7d455a73d6c9b35e02611a6c6ee
diguage <leejun119@gmail.com>
2013-08-05 17:50:26 +0800

    测试中字的字符、字节

---
+++ Article_06/src/com/diguage/pusslers/article_06/ChineseTest.java
@@ -0,0 +1,36 @@
+package com.diguage.pusslers.article_06;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-5
+ * Time: 下午5:37
+ */
+public class ChineseTest {
+    public static void main(String[] args) {
+        // 将一个中文汉字赋值给一个char变量
+        char a = '中';
+        char b = '文';
+        char c = '测';
+        char d = '试';
+        char e = '成';
+        char f = '功';
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.print(e);
+        System.out.print(f);
+
+        System.out.println("------------------");
+
+        // 将一个英文字母赋值给一个byte变量
+        byte ba = 'a';
+
+        // 将一个中文汉字赋值给一个byte变量时，编译会报错
+//        byte bb = '中';
+
+        System.out.println(" byte ba = " + ba);
+//        System.out.println(" byte bb = " + bb);
+    }
+}
