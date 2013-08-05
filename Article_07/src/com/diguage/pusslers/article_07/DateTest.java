55b407dcab04c11cd22808bfb960e168559aff54
diguage <leejun119@gmail.com>
2013-08-05 22:04:42 +0800

    默认时间输出

---
+++ Article_07/src/com/diguage/pusslers/article_07/DateTest.java
@@ -0,0 +1,16 @@
+package com.diguage.pusslers.article_07;
+
+import java.util.Date;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * DateTest: 13-8-5
+ * Time: 下午7:24
+ */
+public class DateTest {
+    public static void main(String[] args) {
+        Date d = new Date();
+        // 在默认时区下输出日期和时间值
+        System.out.println(d);
+    }
+}
