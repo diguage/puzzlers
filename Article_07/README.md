40727132dc14035d6885563bed792de370179d4c
diguage <leejun119@gmail.com>
2013-08-06 12:34:27 +0800

    总结基本类型的一些知识

--- Article_07/README.md
+++ Article_07/README.md
@@ -5,3 +5,4 @@ Java中的月份是从0开始的，它用来表示月份的数值总是比实际
 Calendar下的一堆常量。
 
 在DateFormat参数中，如果需要输出英文字母，则使用单引号括起来，否则会报异常：java.lang.IllegalArgumentException。
+
---
+++ Article_08/src/com/diguage/pusslers/article_08/PrimitiveTest.java
@@ -0,0 +1,20 @@
+package com.diguage.pusslers.article_08;
+
+/**
+ * User: D瓜哥，http://www.diguage.com/
+ * Date: 13-8-6
+ * Time: 下午12:19
+ */
+public class PrimitiveTest {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("基本类型：byte\n二进制位数：" + Byte.SIZE);
+        System.out.println("包装类：java.lang.Byte");
+        System.out.println("最小值：Byte.MIN_VALUE = " + Byte.MIN_VALUE);
+        System.out.println("最大值：Byte.MAX_VALUE = " + Byte.MAX_VALUE);
+        System.out.println();
+
+
+
+    }
+}
